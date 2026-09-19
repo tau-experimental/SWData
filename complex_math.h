ed257c66a71030cd3dbf0621c317e78f462e057f
tau-experimental <invisible.kincajou@gmail.com>
2026-09-19 16:44:16 +0300

    Rethinking all over again

--- complex_math.h
+++ complex_math.h
@@ -2,17 +2,20 @@
 #define COMPLEX_MATH_H
 #include <stdint.h>
 
-/* Комплексное число с плавающей точкой (для ПК-модели высокой точности) */
+/* Комплексное число с плавающей точкой (для вычислений повышенной точности с широкой динамикой) */
 typedef struct {
-    float re;
-    float im;
+    float re, im;
 } cplx_f32;
 
-/* Комплексное число с фиксированной точкой (для будущего переноса на CH32V307) */
+/* Комплексное число с фиксированной точкой: Q15 или целочисленный отсчет АЦП */
 typedef struct {
-    short re; /* Q15 или целочисленный отсчет АЦП */
-    short im;
+    uint16_t re, im;
 } cplx_i16;
+
+/* Комплексное число с фиксированной точкой: Q31 */
+typedef struct {
+	int32_t re, im;
+} cplx_i32;
 
 /* Инициализация комплексного числа */
 cplx_f32 cplx_set(float re, float im);
@@ -42,10 +45,6 @@ float cplx_phase(cplx_f32 in);
 /* Использует аппаратный корень на ПК и аппаратный fsqrt.s на CH32V307 */
 float cplx_mag(cplx_f32 in);
 
-typedef struct {
-	int32_t re, im;
-} cplx_i32;
-
-extern /*inline*/ cplx_i32 cplx_i32_mul_conj(cplx_i16 a, cplx_i16 b);
+extern cplx_i32 cplx_i32_mul_conj(cplx_i16 a, cplx_i16 b);
 
 #endif /* COMPLEX_MATH_H */
